8a3f647e1f58a44eeb701a89d703ef76c0f510a6
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-20 20:15:51 -0300

    Initial Commit

---
+++ Catherine-viagens/Catherine_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catherine_viagensApp.swift
+//  Catherine-viagens
+//
+//  Created by Sósthenes Oliveira Lima on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Catherine_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Catherine-viagens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Catherine-viagens
+//
+//  Created by Sósthenes Oliveira Lima on 20/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+ #Preview {
+    ContentView()
+}
