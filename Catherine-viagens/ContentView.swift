f9ff3ada74e137ef65fa0b3598aa9dc6b23ca68c
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-24 16:29:20 -0300

    Commit - Posição e tamanho com Geometry Reader

--- Catherine-viagens/ContentView.swift
+++ Catherine-viagens/ContentView.swift
@@ -9,33 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Catherine Viagens")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 20))
-                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .center)
-            Text("ESPECIAL")
-                .foregroundStyle(Color.white)
-                .font(.custom("Avenir Book", size: 20))
-                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            Text("BRASIL")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 23))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-        }
-        .background(Color.purple)
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         
-        List {
-            Text("Rio de Janeiro")
-            Text("Ceará")
-            Text("Atibaia")
-            Text("Rio de Janeiro")
+        GeometryReader { view in
             
+            // Lima - VStack principal
+            
+            VStack {
+                
+                // VStack Header
+                
+                VStack {
+                    Text("Catherine Viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .center)
+                    Text("ESPECIAL")
+                        .foregroundStyle(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                    
+                }
+            }
         }
     }
+
     
     
 }
